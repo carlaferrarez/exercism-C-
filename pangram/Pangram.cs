2aa4f9ccb6904254f5e460c960dc2ae88a8ded5d
carlaferrarez <carlacristinef@gmail.com>
2020-04-14 22:48:21 -0300

    adding new challenges

---
+++ pangram/Pangram.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        return input.ToLower().Where(x => char.IsLetter(x)).GroupBy(x => x).Count() == 26;
+    }
+}
